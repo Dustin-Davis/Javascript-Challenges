64c2a6bfcb0c6973fb04cda6bb26f212be990cf1
Dustin-Davis <drpookydavis@comcast.net>
2022-06-23 20:12:56 -0700

    reversed a string

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@
 // For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24.
 // For the test cases, the range will be between 1 and 18 and the input will always be an integer.
 function FirstFactorial(num) {
-  var factorial = 1;
-  for (var i = 1; i <= num; i++) {
+  let factorial = 1;
+  for (let i = 1; i <= num; i++) {
     factorial *= i;
   }
   return factorial;
 }
 console.log(FirstFactorial());
+
+// Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+// For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+function FirstReverse(str) {
+  return str.split("").reverse().join("");
+}
+console.log(FirstReverse());
