186a560413d417ac309f524f3922485a181399e2
Dustin-Davis <drpookydavis@comcast.net>
2022-06-27 18:27:36 -0700

    wrote a function to check to see if a string had the same amount of o,x

--- js/main.js
+++ js/main.js
@@ -16,3 +16,23 @@ function FirstReverse(str) {
   return str.split("").reverse().join("");
 }
 console.log(FirstReverse());
+
+// Check to see if a string has the same amount of 'x's and 'o's.
+// The method must return a boolean and be case insensitive.
+// The string can contain any char.
+
+function XO(str) {
+  var x = 0,
+    o = 0;
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() === "x") {
+      x++;
+    } else if (str[i].toLowerCase() === "o") {
+      o++;
+    }
+  }
+  return x === o;
+}
+
+console.log(XO());
