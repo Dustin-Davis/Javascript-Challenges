07b4e28da4c78733bcd14a528d96a178ac74cd7f
Dustin-Davis <drpookydavis@comcast.net>
2022-07-06 18:53:13 -0700

    found square

--- js/main.js
+++ js/main.js
@@ -121,3 +121,11 @@ function arrayDiff(a, b) {
 }
 
 console.log(arrayDiff())
+
+// Given an integral number,
+// determine if it's a square number
+
+var isSquare = function (n) {
+  return Math.sqrt(n) % 1 === 0
+}
+console.log(isSquare())
